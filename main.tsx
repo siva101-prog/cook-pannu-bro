ab17536032f9492f023e72d840ef9e2fe3bf993a
siva101-prog <sec25cs101@sairamtap.edu.in>
2026-04-19 21:30:38 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import AuthPage from './AuthPage'; // Must match the capital 'A' and 'P'
-import DashboardPage from "./pages/DashboardPage";
-import CookPage from "./pages/CookPage";
+// REMOVE the "/pages/" part because your files are in the root!
+import LandingPage from "./LandingPage"; 
+import AuthPage from './AuthPage'; 
+import DashboardPage from "./DashboardPage";
+import CookPage from "./CookPage";
 
 export default function App() {
   return (
